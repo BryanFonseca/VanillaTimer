08525bb0b4a782cb1999b61365f64b7cb91dbcef
Isaac Fonseca <isaac1805@hotmail.com>
2021-08-01 12:15:58 -0500

    Starting, ticking and pausing

--- app.js
+++ app.js
@@ -7,11 +7,21 @@ class Timer
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
   }
-  start()
+  start = () =>
   {
-    console.log('Starting timer');
-  }
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+  tick = () => 
+  {
+    console.log('tick');
+  };
+  pause = () =>
+  {
+    clearInterval(this.interval);
+  };
 }
 
 const durationInput = document.querySelector('#duration');
